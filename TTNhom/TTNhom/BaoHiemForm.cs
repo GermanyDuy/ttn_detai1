36909ced93e36528286aab4b27c7195a239c098f
hoducduy99 <hoducduy99@gmail.com>
2020-04-23 14:10:03 +0700

    fix loi form bao hiem

--- TTNhom/TTNhom/BaoHiemForm.cs
+++ TTNhom/TTNhom/BaoHiemForm.cs
@@ -14,44 +14,5 @@ namespace TTNhom {
             InitializeComponent();
         }
 
-        private void button7_MouseLeave(object sender , EventArgs e) {
-
-        }
-
-        private void button7_MouseMove(object sender , MouseEventArgs e) {
-
-        }
-
-        private void dataGridView1_CellClick(object sender , DataGridViewCellEventArgs e) {
-
-        }
-
-        private void button6_Click(object sender , EventArgs e) {
-
-        }
-
-        private void button5_Click(object sender , EventArgs e) {
-
-        }
-
-        private void button3_Click(object sender , EventArgs e) {
-
-        }
-
-        private void button1_Click(object sender , EventArgs e) {
-
-        }
-
-        private void button2_Click(object sender , EventArgs e) {
-
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender , EventArgs e) {
-
-        }
-
-        private void textBox2_TextChanged(object sender , EventArgs e) {
-
-        }
     }
 }
